a7379ba197d5070ec103220cbd5ce9ae2b1ad6bc
Dhruvil <Dhruvil@buildesk.in>
2024-04-26 12:56:07 +0530

    cdr.detach( ) usage

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,23 +1,32 @@
-import { Component, OnInit, Input, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy, ChangeDetectorRef, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  // changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ChildComponent implements OnInit {
+export class ChildComponent implements OnInit, OnChanges {
 
   @Input() user: any;
   num: number = 0;
 
-  constructor(private cdr:ChangeDetectorRef) { }
+  constructor(private cdr: ChangeDetectorRef) {
+    // below code tell angular to detach this comp from change detection, event @Input data we cant get
+    cdr.detach();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    // if you use cdr.detach(); and want Input bound data then we have to tell angular in ngOnChange that detect change
+    this.cdr.detectChanges();
+  }
+
 
   ngOnInit(): void {
     // setInterval not work with onPush without telling angular to detect change by this.cdr.detectChanges();
     setInterval(() => {
       this.num = this.num + 1;
-      this.cdr.detectChanges();
+      // this.cdr.detectChanges();
     }, 100);
   }
 
