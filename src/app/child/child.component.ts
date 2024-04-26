4fe6e3085c629396ddae508ef888cc0de628f635
Dhruvil <Dhruvil@buildesk.in>
2024-04-26 13:03:22 +0530

    diff bt detach and onPush

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -18,15 +18,17 @@ export class ChildComponent implements OnInit, OnChanges {
 
   ngOnChanges(changes: SimpleChanges): void {
     // if you use cdr.detach(); and want Input bound data then we have to tell angular in ngOnChange that detect change
+    // if below code not here than num value start from 1 if you use cdr.detach(); 
     this.cdr.detectChanges();
   }
 
 
   ngOnInit(): void {
-    // setInterval not work with onPush without telling angular to detect change by this.cdr.detectChanges();
+    // setInterval, setTimeout, promise, subscription not work with onPush without telling angular to detect change by this.cdr.detectChanges();
+    // ChangeDetectionStrategy.OnPush show 0 value of num as well
     setInterval(() => {
       this.num = this.num + 1;
-      // this.cdr.detectChanges();
+      this.cdr.detectChanges();
     }, 100);
   }
 
